6ef513427a6d9153e3496ca20de3c7a66acc8e23
Chris H <kekaneone@gmail.com>
2020-05-24 21:10:57 -0600

    finish game

--- assets/quiz.js
+++ assets/quiz.js
@@ -1,9 +1,8 @@
 var timeRemaining = 60;
 let score = 0;
-let highScore =0;
+let highScores = [];
 let currentQuestionIndex = 0;
-
-
+var countInterval;
 var questionData = [
     {
         "question": "What was the first game released on N64?",
@@ -15,53 +14,53 @@ var questionData = [
         }
     },
     {
-        "question": "2What was the first game released on N64?",
+        "question": "What is the 2nd best selling game on N64?",
         "answers": {
-            "Super Mario 64": true,
-            "Zelda Ocarina of Time": false,
-            "Mario Kart 64": false,
             "GoldenEye 007": false,
+            "Super Mario 64": false,
+            "Zelda Ocarina of Time": false,
+            "Mario Kart 64": true,
+
         }
     },
     {
-        "question": "3What was the first game released on N64?",
+        "question": "What is the name of the James Bond Game?",
         "answers": {
-            "Super Mario 64": true,
+            "GoldenEye 007": true,
+            "Super Mario 64": false,
             "Zelda Ocarina of Time": false,
             "Mario Kart 64": false,
-            "GoldenEye 007": false
         }
     },
     {
-        "question": "4What was the first game released on N64?",
+        "question": "What Game features the character Link",
         "answers": {
-            "Super Mario 64": true,
-            "Zelda Ocarina of Time": false,
             "Mario Kart 64": false,
+            "Super Mario 64": false,
+            "Zelda Ocarina of Time": true,
             "GoldenEye 007": false
         }
     }
-]
-
+];
 
 function selectAnswer(event) {
     if (questionData[currentQuestionIndex].answers[event.target.innerText]) {
-        console.log('right');
         score += 100;
-    }
-    else {
-        console.log("wrong");
-        score -= 50;
+        updateScore();
+    } else {
+        timeRemaining -= 15;
     }
     if (currentQuestionIndex === 3) {
-        console.log("game over");
+        endGame();
         return;
     }
     currentQuestionIndex++;
     showQuestion(currentQuestionIndex);
 }
 
-
+function updateScore() {
+    document.getElementById("score").getElementsByTagName("span")[0].innerText = score
+}
 
 function startQuiz() {
     document.getElementById("start-page").style.display = "none";
@@ -70,29 +69,67 @@ function startQuiz() {
     showQuestion(currentQuestionIndex);
 }
 
-function showQuestion (i) {
+function showQuestion(i) {
     document.getElementById("question").innerText = questionData[i].question;
-
-        let currAnnswerIndex = 0;
-        for (let answer in questionData[i].answers) {
-            document.getElementById("a-" + currAnnswerIndex).innerText = answer;
-            if (currAnnswerIndex === 3) {
-                break;
-            }
-            currAnnswerIndex++;
+    let currAnnswerIndex = 0;
+    for (let answer in questionData[i].answers) {
+        document.getElementById("a-" + currAnnswerIndex).innerText = answer;
+        if (currAnnswerIndex === 3) {
+            break;
         }
+        currAnnswerIndex++;
+    }
 }
-
 
 function countDown() {
-    var x = setInterval(function () {
+    countInterval = setInterval(function () {
         timeRemaining--;
         document.getElementById("timer").getElementsByTagName("span")[0].innerText = timeRemaining;
-        if (timeRemaining === 0) {
-            console.log("game over");
-            clearInterval(x); //game over
+        if (timeRemaining <= 0) {
+            endGame();
         }
     }, 1000);
-
 }
 
+function endGame() {
+    clearInterval(countInterval); //stop the counter
+    document.getElementById("quiz-page").style.display = "none";
+    document.getElementById("save-page").style.display = "block";
+    document.getElementById("score-label").innerText = "Your score: " + score;
+}
+
+function submitScore() {
+    const initials = document.getElementById("initials").value;
+    if(initials.length == 0) {
+        alert("invalid input");
+        return;
+    }
+    highScores.push({score: score, initials: initials});
+    highScores = highScores.sort((a, b) => b.score - a.score); //sort by high score
+    //reset all values back to default
+    timeRemaining = 60;
+    score = 0;
+    currentQuestionIndex = 0;
+    updateScore();
+    document.getElementById("save-page").style.display = "none";
+    document.getElementById("start-page").style.display = "block";
+    loadHighScores();
+}
+
+window.onload = function () {
+    loadHighScores();
+}
+
+function loadHighScores() {
+    const scoreListDiv = document.getElementById("scores-list");
+    scoreListDiv.innerHTML = ""; //reset scores
+    if (highScores.length > 0) {
+        for (let i = 0; i < highScores.length && i < 5; i++) {
+            const scoreDiv = document.createElement("div");
+            scoreDiv.innerText = highScores[i].initials + " -- score: " + highScores[i].score;
+            scoreListDiv.append(scoreDiv);
+        }
+    } else {
+        scoreListDiv.innerHTML = "No High Scores Yet";
+    }
+}
